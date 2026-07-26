a45b388085eec64a84d584af3ef03ac7ca509a83
daywee-zx <daywillm@gmail.com>
2026-07-26 10:51:46 +0300

    Initial tests

---
+++ tests/misc.go
@@ -0,0 +1,29 @@
+package tests
+
+import (
+	"database/sql"
+	"photoLibrary/pkg/storage"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	testDBPath     = "test_images/test.db"
+	testImageBatch = 9
+)
+
+func setupTestDB(t *testing.T) *storage.Storage {
+	db, err := sql.Open("sqlite", "file:testdb?mode=memory&cache=shared")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	store := storage.NewStorage(db)
+	require.NoError(t, store.Init())
+
+	t.Cleanup(func() {
+		store.Close()
+	})
+	return store
+}
